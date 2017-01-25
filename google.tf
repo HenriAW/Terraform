074f0758a86ba8094cd6a2f442b5e64142ecfe20
Henri Williams <henriwilliams@Henris-MacBook-Pro.local>
2017-01-25 15:56:36 +0000

    Have recreated my gcloud VM setup using Terraform

--- google.tf
+++ google.tf
@@ -8,37 +8,36 @@ provider "google" {
 
 // I now want to create an instance
 
+resource "google_compute_disk" "yolo" {
+  name	= "test-disk"
+  type	= "pd-ssd"
+  zone	= "asia-northeast1-c"
+  size	= 80
+}
+
 resource "google_compute_instance" "default" {
   name         = "terraform-instance"
-  machine_type = "n1-standard-1"
+  machine_type = "custom-4-16384"	#(To create a custom machiene with 2 CPUs and 16GB of memory)
   zone         = "asia-northeast1-c"
 
-  tags = ["foo", "bar"]
-
+  description = "A GCE VM setup with terraform"
+  
   disk {
-    image = "debian-cloud/debian-8"
+    image = "ubuntu-os-cloud/ubuntu-1604-lts" # image-project/image-family
   }
 
-  // Local SSD disk
   disk {
-    type    = "local-ssd"
-    scratch = true
+    disk = "test-disk"
   }
+
+  tags = ["browser-apps"]
 
   network_interface {
     network = "default"
-    access_config {
-      // Ephemeral IP
+  access_config {	# This allows you to ssh into the machiene which is of course very useful!
     }
   }
 
-  metadata {
-    foo = "bar"
-  }
-
-  metadata_startup_script = "echo hi > /test.txt"
-
-  service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-  }
+  metadata_startup_script = "${file("bootstrap.sh")}"
+  
 }
